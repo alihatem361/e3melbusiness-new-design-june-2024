19d5b041d39eb527f41177b87f7135c70c451b9c
Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2024-03-17 09:18:22 +0200

    adding header and primary assets to project

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,10 +1,17 @@
 $(function () {
   const header_start_part = $("#header_start_part");
-  const header_search_dismiss_btn = $("#header_search_dismiss_btn");
+  const header_start_part_js = document.getElementById("header_start_part");
   const search_form_input_lg = $("#search_form_input_lg");
+  const search_form_input_sm = $("#search_form_input_sm");
   const header_search_typeahead_container = $(
     "#header_search_typeahead_container"
   );
+  const mobile_header_search_typeahead_container = $(
+    "#mobile_header_search_typeahead_container"
+  );
+  const search_input_sm_clear_btn = $("#search_input_sm_clear_btn");
+  const mobile_header_menu = $("#mobile_header_menu");
+  const mobile_header_toggler = $("#mobile_header_toggler");
 
   $("#search_form_input_lg").on("focus", function () {
     $(this).addClass("collapsed");
@@ -24,19 +31,82 @@ $(function () {
     header_start_part?.removeClass("collapsed");
   });
 
-  $("#search_form_input_lg").on("keyup", function () {
+  $("#search_form_input_lg, #search_form_input_sm").on("keyup", function () {
     handleTypeahead();
   });
 
-  $("#search_form_input_lg").on("change", function () {
+  $("#search_form_input_lg, #search_form_input_sm").on("change", function () {
     handleTypeahead();
   });
 
   function handleTypeahead() {
-    if (search_form_input_lg?.val()) {
+    if (search_form_input_lg?.val() || search_form_input_sm?.val()) {
+      search_form_input_lg?.addClass("hasValue");
+      search_form_input_sm?.addClass("hasValue");
       header_search_typeahead_container?.addClass("shown");
+      mobile_header_search_typeahead_container?.addClass("shown");
     } else {
       header_search_typeahead_container?.removeClass("shown");
+      mobile_header_search_typeahead_container?.removeClass("shown");
+      search_form_input_lg?.removeClass("hasValue");
+      search_form_input_sm?.removeClass("hasValue");
     }
   }
+
+  if (search_input_sm_clear_btn?.length) {
+    search_input_sm_clear_btn.on("click", function (e) {
+      e.preventDefault();
+      search_form_input_sm?.val("");
+      header_search_typeahead_container?.removeClass("shown");
+      mobile_header_search_typeahead_container?.removeClass("shown");
+      search_form_input_lg?.removeClass("hasValue");
+      search_form_input_sm?.removeClass("hasValue");
+    });
+  }
+
+  $("#header_start_part .nav-item").mouseenter(function (e) {
+    const link = $(this)?.find(".nav-link")?.first();
+    if (link?.length && link?.hasClass("has-submenu")) {
+      const menu = link?.next();
+      menu.css({
+        top: header_start_part_js?.getBoundingClientRect().bottom,
+        left: header_start_part_js?.getBoundingClientRect().left,
+      });
+      menu?.addClass("shown");
+    }
+  });
+  $("#header_start_part .nav-item").mouseleave(function (e) {
+    const link = $(this)?.find(".nav-link")?.first();
+    if (link?.length && link?.hasClass("has-submenu")) {
+      const menu = link?.next();
+      menu.css({
+        top: header_start_part_js?.getBoundingClientRect().bottom,
+        left: header_start_part_js?.getBoundingClientRect().left,
+      });
+      menu?.removeClass("shown");
+    }
+  });
+
+  // Mobile Scripts
+
+  if (mobile_header_toggler?.length && mobile_header_menu) {
+    mobile_header_toggler.on("click", function (e) {
+      e.preventDefault();
+      e.stopPropagation();
+      mobile_header_menu.toggleClass("shown");
+    });
+  }
+
+  $(".mobile_header_link.has-submenu").on("click", function (e) {
+    e.preventDefault();
+    const menu = $(this).next();
+    if (menu?.length) {
+      menu.addClass("shown");
+    }
+  });
+
+  $(".mobile-header-submenu .back-icon").on("click", function (e) {
+    e.preventDefault();
+    $(".mobile-header-submenu").removeClass("shown");
+  });
 });
--- assets/plugins/country-selector/index.js
+++ assets/plugins/country-selector/index.js
@@ -1,3 +1,134 @@
-fetch("assets/plugins/country-selector/data.json")
-  .then((res) => res?.json())
-  .then((data) => console.log(data));
+import { countries } from "./country.js";
+
+const backdrop = document.getElementById("country_overlay_backdrop");
+const overlay = document.getElementById("country_overlay");
+const inner_content = document.getElementById("country_content_inner");
+const lang = document?.getElementsByTagName("html")[0]?.getAttribute("lang");
+const country_overlay_selected_label = document.getElementsByClassName(
+  "country_overlay_selected_label"
+);
+
+if (backdrop && overlay) {
+  let allCountries = [...countries];
+  const currentLangInfo = allCountries?.filter((c) => c.lang === lang)[0];
+  setSelectedCountryInfo(
+    currentLangInfo.lang,
+    currentLangInfo.dir,
+    currentLangInfo.name,
+    currentLangInfo.name_ar,
+    currentLangInfo.flag,
+    currentLangInfo?.arabic
+  );
+  setHTML();
+  backdrop.addEventListener("click", function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    backdrop.classList.remove("shown");
+    overlay.classList.remove("shown");
+  });
+  overlay.addEventListener("click", function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+  });
+  overlay
+    ?.querySelector(".country-search-input")
+    ?.addEventListener("keyup", searchCountries);
+
+  document
+    .getElementById("country_overlay_dismiss_btn")
+    ?.addEventListener("click", hideCountryOverlay);
+
+  function searchCountries() {
+    inner_content.innerHTML = null;
+    allCountries = [...countries];
+    if (this.value) {
+      allCountries = allCountries?.filter(
+        (c) =>
+          c.name?.toLowerCase()?.includes(this.value) ||
+          c.name_ar?.toLowerCase()?.includes(this.value) ||
+          c.lang?.toLowerCase()?.includes(this.value)
+      );
+    } else allCountries = [...countries];
+    setHTML();
+  }
+
+  function setHTML() {
+    allCountries?.forEach((ctry) => {
+      const el = document.createElement("div");
+      el.classList.add("col-lg-4");
+      el.classList.add("col-sm-6");
+      el.innerHTML = `
+      <div class="country-item" data-dir="${ctry?.dir}" data-lang="${
+        ctry?.lang
+      }" data-arabic="${ctry?.arabic}" data-name="${
+        ctry?.name
+      }" data-ar-name="${ctry?.name_ar}" data-img="${ctry?.flag}">
+        <img
+          src="${ctry?.flag}"
+          alt="${ctry?.name}"
+        />
+        <span>${lang === "ar" ? ctry?.name_ar : ctry?.name}</span>
+      </div>
+      `;
+      inner_content?.appendChild(el);
+    });
+    const elements = inner_content?.querySelectorAll(".country-item");
+    for (let i = 0; i < elements?.length; i++) {
+      elements[i].removeEventListener("click", handleCountryClick);
+    }
+    for (let i = 0; i < elements?.length; i++) {
+      elements[i].addEventListener("click", handleCountryClick);
+    }
+  }
+
+  const chng_ctry_btns = document.getElementsByClassName("change_ctry_btn");
+  for (let i = 0; i < chng_ctry_btns.length; i++) {
+    chng_ctry_btns[i].addEventListener("click", showCountryOverlay);
+  }
+
+  function showCountryOverlay() {
+    backdrop.classList.add("shown");
+    overlay.classList.add("shown");
+  }
+
+  function hideCountryOverlay() {
+    backdrop.classList.remove("shown");
+    overlay.classList.remove("shown");
+    inner_content.innerHTML = "";
+  }
+
+  // ****************************************************************************************************
+  // ****************************************************************************************************
+  // ****************************************************************************************************
+  // Work Here
+
+  function handleCountryClick(event) {
+    event.preventDefault();
+    event.stopPropagation();
+    const lang = this.getAttribute("data-lang");
+    const dir = this.getAttribute("data-dir");
+    const langName = this.getAttribute("data-name");
+    const langARName = this.getAttribute("data-ar-name");
+    const img = this.getAttribute("data-img");
+    const arabic = this.getAttribute("data-arabic");
+    setSelectedCountryInfo(lang, dir, langName, langARName, img, eval(arabic));
+    hideCountryOverlay();
+  }
+
+  function setSelectedCountryInfo(
+    lang,
+    dir,
+    langName,
+    langARName,
+    img,
+    arabic
+  ) {
+    for (let i = 0; i < country_overlay_selected_label.length; i++) {
+      country_overlay_selected_label[i].getElementsByTagName("img")[0].src =
+        img;
+      country_overlay_selected_label[i].getElementsByTagName(
+        "small"
+      )[0].innerText = arabic ? langARName : langName;
+    }
+  }
+}
