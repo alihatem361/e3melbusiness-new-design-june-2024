1736cca3a4a37af3c1d23afc775916b64b1898f2
Ahmed Aly Mohamed <ahmedalimohamed1994@gmail.com>
2024-11-14 18:29:00 +0200

    single course changes, updating styles

--- assets/js/carousel.js
+++ assets/js/carousel.js
@@ -1,36 +1,80 @@
-new Swiper(".product_swiper_1", {
-  effect: "coverflow",
-  centeredSlides: true,
-  coverflowEffect: {
-    rotate: 50,
-    stretch: 0,
-    depth: 100,
-    modifier: 1,
-    slideShadows: false,
-  },
-  slidesPerView: 2,
-  spaceBetween: 0,
-  grabCursor: false,
-  loop: true,
-  mousewheel: false,
-  allowTouchMove: true,
-  lazy: true,
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    0: {
+(function () {
+  if ($('.product_swiper_1').length) {
+    new Swiper('.product_swiper_1', {
+      effect: 'coverflow',
+      centeredSlides: true,
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: false,
+      },
       slidesPerView: 2,
-    },
-    576: {
-      slidesPerView: 3,
-    },
-    1024: {
-      slidesPerView: 4,
-    },
-    1200: {
-      slidesPerView: 5.5,
-    },
-  },
-});
+      spaceBetween: 0,
+      grabCursor: false,
+      loop: true,
+      mousewheel: false,
+      allowTouchMove: true,
+      lazy: true,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 2,
+        },
+        576: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+        1200: {
+          slidesPerView: 5.5,
+        },
+      },
+    });
+  }
+
+  if ($('.courses-swiper-container-v1').length) {
+    new Swiper('.courses-swiper-container-v1', {
+      slidesPerView: 1,
+      spaceBetween: 20,
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+      breakpoints: {
+        0: {
+          slidesPerView: 1.25,
+        },
+        576: {
+          slidesPerView: 2,
+        },
+        992: {
+          slidesPerView: 2.5,
+        },
+        1200: {
+          slidesPerView: 4,
+        },
+      },
+    });
+  }
+
+  if ($('.video-testimonial-swiper-container').length) {
+    new Swiper('.video-testimonial-swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: '#video-testimonial-swiper-btn-next',
+        prevEl: '#video-testimonial-swiper-btn-prev',
+      },
+      autoplay: {
+        delay: 3000,
+        disableOnInteraction: true,
+      },
+    });
+  }
+})();
